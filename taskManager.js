c95f38e3341bad150073e6be670d35cae434a895
kasunihansani <kasunihansani7@gmail.com>
2025-11-25 13:28:38 +0530

    Updated

--- taskManager.js
+++ taskManager.js
@@ -1,11 +1,14 @@
-//creating readline interface
-import readline from 'readline';
+//Import Node.js's "readline" module
 import { createInterface } from 'readline';
+
+//Creating readline interface to ask questions and get user input in the terminal
 const rl = createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+//Wraps "rl.question()" inside a Promise to use "await" later
+//When user types somthing, it resolves the answer
 function ask(question) {
     return new Promise(resolve => {
         rl.question(question, (answer) => {
@@ -14,23 +17,28 @@ function ask(question) {
     });
 }
 
-let j = 0;
-const tasks = [];
-const completed = [];
-let task;
 
+const tasks = []; //Stores task names
+const completed = []; //Store true or false for each task(true = completed)
+
+//main() function
+//Runs the program in a loop until user selects Exit
 async function main() {
     console.log("Welcome to Task Manager!");
     while (true) {
 
+        //Print menu
         console.log("\n1. List tasks");
         console.log("2. Add new task");
         console.log("3. Mark task as complete");
         console.log("4. Delete task");
         console.log("5. Exit");
 
-        let num = await ask("Enter your choice: ");
-        num = parseInt(num);
+        //Get user choice
+        let num = await ask("Enter your choice: "); // Wait for a input
+        num = parseInt(num);// Convert string to int
+        
+        //Handle the menu according to user input
         if (num === 1) {
             listTask();
         }
@@ -49,24 +57,30 @@ async function main() {
         }
         else {
             console.log("\nInvalid input. Try Again.\n");
-            break;
         }
     }
-    rl.close();
+    rl.close(); //Close readline
 }
-
 
 main();
 
+//Show list of tasks
 function listTask() {
+
+    //Inform if the list is empty
     if (tasks.length === 0) {
         console.log("\nList is empty");
         return;
     }
+
+    //Show the list of tasks
     else {
+
+        //Print each task
         console.log("\nYour Tasks: ");
         for (let i = 0; i < tasks.length; i++) {
 
+            //Show whether the task is completed [X] or not [X]
             let mark;
 
             if (completed[i]===true) {
@@ -75,9 +89,9 @@ function listTask() {
             else{
                 mark = "[ ]";
             }
-            console.log(`${i+1}. ${mark} ${tasks[i]}`);
+            console.log(`${i+1}. ${mark} ${tasks[i]}`);//Print formatted task
 
-            //console.log();
+
             /* If use forEach.
             tasks.forEach((task, index) => {
             console.log(`${index + 1}. ${task}`);
@@ -87,6 +101,7 @@ function listTask() {
     }
 }
 
+//Add new task
 async function addTask() {
 
     let newTask = await ask("\nEnter new task name: ");
@@ -96,30 +111,34 @@ async function addTask() {
 
 }
 
+//Mark tasks as complete
 async function completeTask() {
     let num = await ask("Enter task number to mark complete: ");
     num = parseInt(num);
 
+    //Validate number
     if (num < 1 || num > tasks.length) {
         console.log("Invalid task number.");
         return;
     }
 
-    completed[num-1] = true;
+    completed[num-1] = true;//Set the completed task true
     console.log(`✅ Task ${num} marked as complete!\n`);
 }
 
+//Delete tasks
 async function deleteTask() {
     let num = await ask("Enter task number to delete: ");
     num = parseInt(num);
 
+    //Validate number
     if (num < 1 || num > tasks.length) {
         console.log("Invalid task number.");
         return;
     }
 
-    tasks.splice(num - 1, 1);
-    completed.splice(num - 1, 1);
+    tasks.splice(num - 1, 1); //Remove the item at index
+    completed.splice(num - 1, 1);//Removes the relevent completed status
 
-    console.log(`🗑️ Task ${num} is deleted\n`);
+    console.log(`🗑️  Task ${num} is deleted\n`);//Confirm the deletion
 }
